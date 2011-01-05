e9987b7ef8040186a39fc8bb4e701f0567f37cab
Tom Kay <oridan82@gmail.com>
2011-01-05 16:49:46 +0000

    initial commit

---
+++ fileManager.php
@@ -0,0 +1,212 @@
+<?php
+/*
+* File Manager
+*
+* Copyright (c) 2010 Tom Kay - oridan82@gmail.com
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+*
+*/
+
+class jqFileManager {
+	private static $data = array();
+	static function GetRelativePath($path) {
+		//$path = realpath($path); // dont use realpath as it doesnt work with files that dont exist.
+    $path = preg_replace('/[^\/]+\/\.\.\/?/', '', $path);
+		$pos = strpos($path,$_SERVER['DOCUMENT_ROOT']);
+		return '/'.ltrim(substr($path,$pos+strlen($_SERVER['DOCUMENT_ROOT'])),DIRECTORY_SEPARATOR);
+	}
+	static function GetPathFolder() {
+		return self::GetRelativePath(dirname(__FILE__)).DIRECTORY_SEPARATOR;
+	}
+	static function GetPathSelf() {
+		return self::GetRelativePath(__FILE__);
+	}
+	static function GetPathJS() {
+		return self::GetPathFolder().'jquery.fileManager.js';
+	}
+	static function GetPathCSS() {
+		return self::GetPathFolder().'jquery.fileManager.css';
+	}
+
+	static function AddIcon($path, $title='',$folder=false) {
+		self::$data[] = array('path'=>$path,'title'=>$title,'type'=>$folder);
+	}
+	static function ProcessAjax($rootPath,$deleteCallback=null,$renameCallback=null) {
+		$pMod = array_key_exists('path',$_GET) ? $_GET['path'] : '';
+		$path = $rootPath.'/'.trim($pMod,'/');
+
+		// translate path
+		$path = preg_replace('/[^\/]+\/\.\.\/?/', '', $path);
+		$path = '/'.trim($path,'/');
+		if (!file_exists($path)) mkdir($path,octdec('0777'),true);
+//		chmod($path,0777);
+
+		if (array_key_exists('delete',$_GET)) {
+			$from = $path.'/'.$_GET['delete'];
+			if (!file_exists($from)) {
+				echo 'alert("File or Folder no longer exists");';
+				return false;
+			}
+			try {
+				if (is_dir($from)) rmdir($from);
+				else unlink($from);
+			} catch (Exception $e) {
+				echo 'alert("Cannot Delete.  Folder may not be empty.");';
+				return false;
+			}
+			return true;
+		}
+		if (array_key_exists('mFrom',$_GET) && array_key_exists('mTo',$_GET)) {
+			$from = $path.'/'.$_GET['mFrom'];
+			$to = $path.'/'.$_GET['mTo'];
+			$to = preg_replace('/\w+\/\.\.\//', '', $to);
+			if (file_exists($to)) {
+				echo 'alert("Destination already exists");';
+				return false;
+			}
+			try {
+				rename($from,$to);
+			} catch(Exception $e) {
+				echo $e->getMessage();
+				echo 'alert("Cannot move or rename.");';
+				return false;
+			}
+			$from = $path.'/'.$_GET['mFrom'];
+			$to = $path.'/'.$_GET['mTo'];
+			if (is_callable($renameCallback)) call_user_func($renameCallback,$from,$to);
+			return true;
+		}
+
+		$glob = glob($path.'/{,.}*',GLOB_BRACE);
+		$files = array_merge(array_filter($glob, 'is_dir'),array_filter($glob, 'is_file'));
+		foreach ($files as $file) {
+			$filename = basename($file);
+			if ($filename === '..' || $filename === '.') continue;
+			if (!is_dir($file) && array_key_exists('filter',$_GET) && !preg_match('/'.$_GET['filter'].'/i',$filename)) continue;
+		//	$fldr = is_dir($file) ? 'cmsMediaFolder cmsDrop cmsDrag' : 'cmsDrag';
+			self::AddIcon($filename,$filename,is_dir($file)?1:0);
+			//echo "<div class=\"cmsMediaIcon$fldr\" title=\"$filename\">$filename</div>";
+		}
+
+		// uPath is full path less rootpath less filename
+		$uPath = substr(self::GetRelativePath($path),strlen(self::GetRelativePath($rootPath)));
+		if (!$uPath) $uPath = '';
+		die(json_encode(array('rootPath'=>self::GetRelativePath($rootPath),'path'=>$uPath,'files'=>self::$data)));
+	}
+
+	public static function ProcessUpload($rootPath) {
+		if (ob_get_level()) ob_end_clean();
+		$pMod = array_key_exists('path',$_GET) ? $_GET['path'] : '';
+		if (!file_exists($rootPath)) mkdir($rootPath);
+		$destination = realpath(rtrim($rootPath,'/').DIRECTORY_SEPARATOR.trim($pMod,'/'));
+
+		// HTTP headers for no cache etc
+		header('Content-type: text/plain; charset=UTF-8');
+		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+		header("Cache-Control: no-store, no-cache, must-revalidate");
+		header("Cache-Control: post-check=0, pre-check=0", false);
+		header("Pragma: no-cache");
+
+		// Settings
+		$targetDir = $destination;// ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
+		$cleanupTargetDir = false; // Remove old files
+		$maxFileAge = 60 * 60; // Temp file age in seconds
+
+		// 5 minutes execution time
+		//set_time_limit(0);
+		// usleep(5000);
+
+		// Get parameters
+		$chunk = array_key_exists('chunk',$_REQUEST) ? $_REQUEST["chunk"] : 0;
+		$chunks = array_key_exists('chunks',$_REQUEST) ? $_REQUEST["chunks"] : 0;
+		$fileName = array_key_exists('name',$_REQUEST) ? $_REQUEST["name"] : '';
+
+		// Clean the fileName for security reasons
+		$fileName = preg_replace('/[^\w\._]+/', '', $fileName);
+		if (is_dir($targetDir . DIRECTORY_SEPARATOR . $fileName)) die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+
+
+		// Create target dir
+		if (!file_exists($targetDir)) {
+			mkdir($targetDir);
+			chmod($targetDir,0777);
+		}
+
+		// Remove old temp files
+		if (is_dir($targetDir) && ($dir = opendir($targetDir))) {
+			while (($file = readdir($dir)) !== false) {
+				$filePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+
+				// Remove temp files if they are older than the max age
+				if (preg_match('/\\.tmp$/', $file) && (filemtime($filePath) < time() - $maxFileAge))
+					unlink($filePath);
+			}
+
+			closedir($dir);
+		} else
+			die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+
+		$contentType = '';
+		// Look for the content type header
+		if (isset($_SERVER["HTTP_CONTENT_TYPE"]))
+			$contentType = $_SERVER["HTTP_CONTENT_TYPE"];
+
+		if (isset($_SERVER["CONTENT_TYPE"]))
+			$contentType = $_SERVER["CONTENT_TYPE"];
+
+		if (strpos($contentType, "multipart") !== false) {
+			if (isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
+				// Open temp file
+				$out = fopen($targetDir . DIRECTORY_SEPARATOR . $fileName, $chunk == 0 ? "wb" : "ab");
+				if ($out) {
+					// Read binary input stream and append it to temp file
+					$in = fopen($_FILES['file']['tmp_name'], "rb");
+
+					if ($in) {
+						while ($buff = fread($in, 4096))
+							fwrite($out, $buff);
+					} else
+						die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+
+					fclose($out);
+					unlink($_FILES['file']['tmp_name']);
+				} else
+					die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+			} else
+				die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+		} else {
+			// Open temp file
+			$out = fopen($targetDir . DIRECTORY_SEPARATOR . $fileName, $chunk == 0 ? "wb" : "ab");
+			if ($out) {
+				// Read binary input stream and append it to temp file
+				$in = fopen("php://input", "rb");
+
+				if ($in) {
+					while ($buff = fread($in, 4096))
+						fwrite($out, $buff);
+				} else
+					die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+
+				fclose($out);
+			} else
+				die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+		}
+
+		// Return JSON-RPC response
+		die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
+	}
+}
+?>
---
+++ jquery.fileManager.js
@@ -0,0 +1,218 @@
+/*
+* File Manager
+*
+* Copyright (c) 2010 Tom Kay - oridan82@gmail.com
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+*
+*/
+
+;(function($){
+	var optionDefaults = {
+		path			: '',
+		upload			: false,
+		readonly		: false,
+		fixedPath		: false,
+		
+		baseClass		: 'fmBase',
+		folderClass		: 'fmFolder',
+		trashClass		: 'fmTrash',
+		loadingClass	: 'fmLoading',
+
+		highlightClass	: 'ui-state-highlight',
+		hoverClass		: 'ui-state-active'
+	};
+	
+	$.fn.fileManager = function(settings, pluploadOptions) {
+		var mbOptions = $.extend({}, optionDefaults, settings);
+	//	mbOptions.events = $.extend({selectstart:function () { return false; },dblclick:ItemDblClick},mbOptions.events);
+//		if (!mbOptions.uploadPath) {
+//			alert('uploadPath not specified'); return;
+//		}
+		if (!mbOptions.ajaxPath) {
+			alert('ajaxPath not specified'); return;
+		}
+		if (mbOptions.fixedPath) mbOptions.path = mbOptions.fixedPath;
+
+		var query = $.extend({},{path:mbOptions.path},mbOptions.get);
+		this.each(function () { // swap with getJSON so not duplicating ajax
+			var $sel = $(this);
+		//	$sel.empty();
+		//	$sel.append('<div>Loading FileManager...</div>');
+			$.getJSON(mbOptions.ajaxPath, query, function(data, status) {
+				//localOptions = mbOptions;
+				$sel.data('result',data);
+//				localOptions.path = data.path;
+//				localOptions.relPath = data.relPath;
+				$sel.data('options',mbOptions);
+				if (status != "success") {
+					var msg = "Sorry but there was an error: "+status;
+				}
+				// process response
+				$sel.empty();
+
+				if (!mbOptions.readonly) {
+					$sel.append('<div>Path: '+data.rootPath+data.path+'</div>');
+					if (data.path && !mbOptions.fixedPath) DrawItem($sel,{path:'..',title:'..',type:1});
+					DrawItem($sel,{path:'',title:'',type:2});
+				}
+				if ($(data.files).length == 0) {
+				  $sel.append('<div style="font-size:1.3em;padding:0.5em">No files in this folder.</div>');
+				}
+				$(data.files).each(function () {
+					DrawItem($sel,this);
+				});
+				$sel.append('<div style="clear:both"></div>');
+				if (!mbOptions.readonly)
+					$sel.append($('<div>New Folder</div>').button().bind('click',NewFolder));
+				if (mbOptions.upload) {
+					var ul = $('<div></div>').hide();
+					$sel.append($('<div>Upload Files</div>').button().bind('click',{container:ul},UploadFiles));
+					$sel.append(ul);
+				}
+				
+				// end processing
+				
+				$('.'+mbOptions.baseClass,$sel).disableSelection();//bind('selectstart',function () { return false; });
+				$('.'+mbOptions.folderClass,$sel).bind('dblclick',ItemDblClick);
+//				DocReady(); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> event
+			});
+
+			function UploadFiles(event) {
+				$(event.data.container).toggle();
+				if (plupload && pluploadOptions) {
+					if (!$(event.data.container).pluploadQueue) {
+						$(event.data.container).html('Must install Plupload jquery plugin.'); return;
+					}
+					var opts = pluploadOptions;
+					opts.url = opts.url+ (opts.url.indexOf('?') < 0 ? '?' : '&') +'path='+$sel.data('result').path;
+					$(event.data.container).pluploadQueue(opts);
+					$(event.data.container).pluploadQueue().bind('FileUploaded',function (uploader,file,response) {
+						if (uploader.total.queued == 0) {
+							RefreshView($sel);
+						}
+					});
+				} else {
+					$(event.data.container).html('Must install Plupload.');
+				}
+			}
+			function NewFolder() {
+				var path = prompt('Enter Folder Name:');
+				if (!path) return;
+				if ($sel.data('result').path) path = $sel.data('result').path + '/' + path;
+				ReloadFolder($sel, path);
+			}
+		});
+
+
+		function FileDropped(event,ui) {
+			var from = $(ui.draggable);
+			var to = $(this);
+
+			if ($(this).hasClass(mbOptions.trashClass)) {
+				if (confirm('Really delete "'+from.data('item').title+'"?  This cannot be undone')) {
+					ajaxData = {path:mbOptions.path,'delete':from.data('item').path};
+					$.ajax({url:mbOptions.ajaxPath,data:ajaxData,dataType:'script',complete:function() {
+						RefreshView(from.data('item').target);
+					}});
+				}
+			} else {
+				return Rename(from.data('item').target,from.data('item').path,to.data('item').path+'/'+from.data('item').path);
+				//ajaxData = {path:mbOptions.path,mFrom:from.data('item').path,mTo:to.data('item').path+'/'+from.data('item').path};
+			}
+		};
+		function Rename(view,from,to) {
+			ajaxData = {path:mbOptions.path,mFrom:from,mTo:to};
+			$.ajax({url:mbOptions.ajaxPath,data:ajaxData,dataType:'script',complete:function() {
+				RefreshView(view);
+			}});
+		}
+		function ItemDblClick() {
+			var item = $(this).data('item');
+			if (item.type != ICONTYPE_FOLDER) return;
+//			$(this).append($('<div></div>').addClass('fmLoading'));
+			var path = item.path;
+			if (item.target.data('result').path) path = item.target.data('result').path + '/' + path;
+			ReloadFolder(item.target,path);
+		}
+		function RefreshView(target) {
+			ReloadFolder(target,target.data('result').path);
+		}
+		function ReloadFolder(target,path) {
+			target.fileManager($.extend({},mbOptions,{path:path}),pluploadOptions);
+		};
+
+		var ICONTYPE_FILE = 0;
+		var ICONTYPE_FOLDER = 1;
+		var ICONTYPE_TRASH = 2;
+		function DrawItem(target, item) {
+			if (item.type == ICONTYPE_FOLDER && mbOptions.fixedPath) return;
+			
+			item.target = $(target);
+			item.fullPath = item.target.data('result').rootPath + item.target.data('result').path + '/' + item.path;
+			var icon = $('<div title="'+item.title+'"></div>');
+			icon.data('item',item);
+			icon.addClass(mbOptions.baseClass);
+
+			// set classes
+			if (item.type == ICONTYPE_FOLDER)
+				icon.addClass(mbOptions.folderClass);
+			else if (item.type == ICONTYPE_TRASH)
+				icon.addClass(mbOptions.trashClass);
+			
+			// set draggables
+			if (!mbOptions.readonly) {
+				if (item.type == ICONTYPE_FOLDER || item.type == ICONTYPE_TRASH)
+					icon.droppable({tolerance:'intersect',accept:'.'+mbOptions.baseClass,drop:FileDropped,hoverClass:mbOptions.hoverClass,activeClass:mbOptions.highlightClass});
+				if (item.type != ICONTYPE_TRASH && item.path != '..')
+					icon.draggable({stack:'files',revert:true,zIndex:1000,opacity:0.5,scroll:false});
+			}
+			
+			if (item.type == ICONTYPE_FILE && item.path.search(/.jpeg|.jpg|.png|.gif|.tif|.tiff/i) > -1) {
+				icon.css('background-image','none');
+				icon.append('<img style="width:100%;height:100%" src="'+item.fullPath+'">');
+			}
+			
+			// events
+			if (mbOptions.events) {
+				$.each(mbOptions.events,function (index,value) {
+					icon.bind(index, value);
+				});
+			}
+			icon.data('result',target.data('result'));
+			
+			target.append(icon);
+			if (!item.path) return;
+
+			//if (mbOptions.readonly) return;
+				
+			var ext = item.path.match(/\.[^.\b]*$/i);
+			if (ext) ext = ext[0];
+			else ext = '';
+			var basename = item.path.replace(ext,'');
+
+			var renamebox = $('<input style="position:absolute;left:-1em;bottom:-1px;right:-1em" type="text">')
+				.bind('focus',function() { $(this).val(basename); })
+				.bind('blur',function() { $(this).hide(); if (item.path == $(this).val()+ext) return; Rename(item.target,item.path,$(this).val()+ext); })
+				.bind('keydown',function(event) { if (event.keyCode == '13' || event.keyCode == '27') $(this).blur(); })
+				.hide()
+				.appendTo(icon);
+			var label = $('<div class="label">'+item.title+'</div>')
+				.prependTo(icon)//.bind('dblclick',function() {return false;})
+				.bind('click',function () { if (!mbOptions.readonly) { renamebox.show().focus(); return false;}});
+		}
+		
+		return $(this);
+	};
+})(jQuery);
